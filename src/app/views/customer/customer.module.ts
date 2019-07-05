1b848ad094901744b639b925ec3f9f4e5a9db052
virendra <virendra150995@gmail.com>
2019-07-05 10:50:36 +0530

    created list in mockservice

--- src/app/views/customer/customer.module.ts
+++ src/app/views/customer/customer.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { CustomerRoutingModule } from './customer-routing.module';
 import { SearchCustomerComponent } from './search-customer/search-customer.component';
-
+import { AngularMultiSelectModule } from 'angular2-multiselect-dropdown';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [SearchCustomerComponent],
   imports: [
     CommonModule,
-    CustomerRoutingModule
+    CustomerRoutingModule,
+    AngularMultiSelectModule,
+    FormsModule
   ]
 })
 export class CustomerModule { }
+
