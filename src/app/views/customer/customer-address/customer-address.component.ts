4ebd7ee848059b9224301955fa20e7429f1a704b
virendra <virendra150995@gmail.com>
2019-07-25 19:03:05 +0530

    resolved address issue when no address found

--- src/app/views/customer/customer-address/customer-address.component.ts
+++ src/app/views/customer/customer-address/customer-address.component.ts
@@ -50,25 +50,34 @@ export class CustomerAddressComponent implements OnInit {
         this.temp = response.customers;
         this.custAddress = [];
         this.getTotalRecord();
-        for (let i = 0; i < response.customers.length; i++) {
-          const data = {
-            firstName: this.temp[i].addresses[0].first_name,
-            lastName: this.temp[i].addresses[0].last_name,
-            email: this.temp[i].addresses[0].email,
-            phoneNo: this.temp[i].addresses[0].phone_number,
-            faxNo: this.temp[i].addresses[0].fax_number,
-            address: new Array(this.temp[i].addresses[0].company,
-              this.temp[i].addresses[0].address1,
-              this.temp[i].addresses[0].address2,
-              this.temp[i].addresses[0].city,
-              this.temp[i].addresses[0].zip_postal_code,
-              this.temp[i].addresses[0].country),
-            customerId: this.temp[0].id,
-            addressId: this.temp[0].addresses[0].id
-          }
-          this.custAddress.push(data);
+        if (this.temp[0].addresses.length) {
 
+          for (let i = 0; i < this.temp.length; i++) {
+
+            if (this.temp[i].addresses.length) {
+              var data = {
+                firstName: this.temp[i].addresses[0].first_name,
+                lastName: this.temp[i].addresses[0].last_name,
+                email: this.temp[i].addresses[0].email,
+                phoneNo: this.temp[i].addresses[0].phone_number,
+                faxNo: this.temp[i].addresses[0].fax_number,
+                address: new Array(this.temp[i].addresses[0].company,
+                  this.temp[i].addresses[0].address1,
+                  this.temp[i].addresses[0].address2,
+                  this.temp[i].addresses[0].city,
+                  this.temp[i].addresses[0].zip_postal_code,
+                  this.temp[i].addresses[0].country),
+                customerId: this.temp[0].id,
+                addressId: this.temp[0].addresses[0].id
+              }
+            }
+            this.custAddress.push(data);
+          }
+        } else {
+          this._cS.displayToast(3, "No address found");
+          this.totalRecords = 0;
         }
+
       })
 
   }
